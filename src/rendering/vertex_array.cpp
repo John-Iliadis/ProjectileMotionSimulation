faabeea12a8356ff1905a716a6b553404686aef0
Ioannis Iliadis <giannisiliadis2k15@gmail.com>
2024-02-19 18:27:07 +1100

    implemented vertex array

---
+++ src/rendering/vertex_array.cpp
@@ -0,0 +1,104 @@
+//
+// Created by Gianni on 19/02/2024.
+//
+
+#include "vertex_array.hpp"
+
+VertexArray::VertexArray()
+    : m_renderer_id()
+    , m_vertex_count()
+    , m_index_count()
+{
+    glGenVertexArrays(1, &m_renderer_id);
+}
+
+VertexArray::~VertexArray()
+{
+    delete_vertex_array();
+}
+
+VertexArray::VertexArray(VertexArray &&other) noexcept
+{
+    m_renderer_id = other.m_renderer_id;
+    m_vertex_count = other.m_vertex_count;
+    m_index_count = other.m_index_count;
+
+    other.delete_vertex_array();
+}
+
+VertexArray &VertexArray::operator=(VertexArray &&other) noexcept
+{
+    if (this != &other)
+    {
+        m_renderer_id = other.m_renderer_id;
+        m_vertex_count = other.m_vertex_count;
+        m_index_count = other.m_index_count;
+
+        other.delete_vertex_array();
+    }
+
+    return *this;
+}
+
+void VertexArray::bind() const
+{
+    glBindVertexArray(m_renderer_id);
+}
+
+void VertexArray::unbind() const
+{
+    glBindVertexArray(0);
+}
+
+void VertexArray::attach_vertex_buffer(const VertexBufferStatic &vbo, const VertexBufferLayout &layout)
+{
+    m_vertex_count = vbo.get_count();
+
+    bind();
+    vbo.bind();
+
+    uint32_t offset{};
+
+    for (const auto& attribute : layout.get_attributes())
+    {
+        glEnableVertexAttribArray(attribute.index);
+        glVertexAttribPointer(attribute.index, attribute.size, attribute.type, attribute.normalized, layout.get_stride(), reinterpret_cast<void*>(offset));
+        offset += attribute.size * utils::gl_sizeof(attribute.type);
+    }
+
+    unbind();
+    vbo.unbind();
+}
+
+void VertexArray::attach_index_buffer(const IndexBufferStatic &ibo)
+{
+    m_index_count = ibo.get_count();
+
+    bind();
+    ibo.bind();
+
+    unbind();
+    ibo.unbind();
+}
+
+uint32_t VertexArray::get_vertex_count() const
+{
+    return m_vertex_count;
+}
+
+uint32_t VertexArray::get_index_count() const
+{
+    return m_index_count;
+}
+
+void VertexArray::delete_vertex_array()
+{
+    if (m_renderer_id)
+    {
+        glDeleteVertexArrays(1, &m_renderer_id);
+
+        m_renderer_id = 0;
+        m_vertex_count = 0;
+        m_index_count = 0;
+    }
+}
---
+++ src/rendering/vertex_array.hpp
@@ -0,0 +1,44 @@
+//
+// Created by Gianni on 19/02/2024.
+//
+
+#ifndef PROJECTILEMOTIONSIM_VERTEX_ARRAY_HPP
+#define PROJECTILEMOTIONSIM_VERTEX_ARRAY_HPP
+
+#include "vertex_buffer_static.hpp"
+#include "index_buffer_static.hpp"
+#include "vertex_buffer_layout.hpp"
+
+
+class VertexArray
+{
+public:
+    VertexArray();
+    ~VertexArray();
+
+    VertexArray(VertexArray&& other) noexcept;
+    VertexArray& operator=(VertexArray&& other) noexcept;
+
+    VertexArray(const VertexArray&) = delete;
+    VertexArray& operator=(const VertexArray&) = delete;
+
+    void bind() const;
+    void unbind() const;
+
+    void attach_vertex_buffer(const VertexBufferStatic& vbo, const VertexBufferLayout& layout);
+    void attach_index_buffer(const IndexBufferStatic& ibo);
+
+    uint32_t get_vertex_count() const;
+    uint32_t get_index_count() const;
+
+private:
+    void delete_vertex_array();
+
+private:
+    uint32_t m_renderer_id;
+    uint32_t m_vertex_count;
+    uint32_t m_index_count;
+};
+
+
+#endif //PROJECTILEMOTIONSIM_VERTEX_ARRAY_HPP
