2cb93dd2af4e0090170802463c5e343cda24eaa3
Ioannis Iliadis <giannisiliadis2k15@gmail.com>
2024-06-07 11:23:58 +1000

    added text rendering includes

---
+++ src/text_rendering/text_rendering_includes.hpp
@@ -0,0 +1,13 @@
+//
+// Created by Gianni on 7/06/2024.
+//
+
+#ifndef PROJECTILEMOTIONSIM_TEXT_RENDERING_INCLUDES_HPP
+#define PROJECTILEMOTIONSIM_TEXT_RENDERING_INCLUDES_HPP
+
+#include "text.hpp"
+#include "font_atlas.hpp"
+#include "text_renderer.hpp"
+
+
+#endif //PROJECTILEMOTIONSIM_TEXT_RENDERING_INCLUDES_HPP
