8783ca2db8157d390f6e8a2712c81db82d459da8
Ioannis Iliadis <124552848+John-Iliadis@users.noreply.github.com>
2024-07-19 08:08:01 +1000

    inlined global constants

--- src/renderer2D/renderer2D_constants.hpp
+++ src/renderer2D/renderer2D_constants.hpp
@@ -11,20 +11,20 @@
 namespace renderer2D::constants
 {
     // quads
-    constexpr uint32_t QUAD_VERTEX_COUNT = 4;
-    constexpr uint32_t QUAD_INDEX_COUNT = 6;
-    constexpr uint32_t MAX_QUADS = 20'000;
-    constexpr uint32_t MAX_QUAD_VERTICES = MAX_QUADS * QUAD_VERTEX_COUNT;
-    constexpr uint32_t MAX_QUAD_INDICES = MAX_QUADS * QUAD_INDEX_COUNT;
+    inline constexpr uint32_t QUAD_VERTEX_COUNT = 4;
+    inline constexpr uint32_t QUAD_INDEX_COUNT = 6;
+    inline constexpr uint32_t MAX_QUADS = 20'000;
+    inline constexpr uint32_t MAX_QUAD_VERTICES = MAX_QUADS * QUAD_VERTEX_COUNT;
+    inline constexpr uint32_t MAX_QUAD_INDICES = MAX_QUADS * QUAD_INDEX_COUNT;
 
     // points
-    constexpr uint32_t MAX_POINTS = 20'000;
-    constexpr uint32_t MAX_POINT_VERTICES = MAX_POINTS;
+    inline constexpr uint32_t MAX_POINTS = 20'000;
+    inline constexpr uint32_t MAX_POINT_VERTICES = MAX_POINTS;
 
     // lines
-    constexpr uint32_t LINE_VERTEX_COUNT = 2;
-    constexpr uint32_t MAX_LINES = 20'000;
-    constexpr uint32_t MAX_LINE_VERTICES = MAX_LINES * LINE_VERTEX_COUNT;
+    inline constexpr uint32_t LINE_VERTEX_COUNT = 2;
+    inline constexpr uint32_t MAX_LINES = 20'000;
+    inline constexpr uint32_t MAX_LINE_VERTICES = MAX_LINES * LINE_VERTEX_COUNT;
 }
 
 
